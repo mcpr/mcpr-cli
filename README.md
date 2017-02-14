caae49137eb6954531e940dd39fcc7a28b4b7a06
Noah Prail <nprail@filiosoft.com>
2017-02-14 15:55:38 -0500

    Let's of improvements.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# mc-cli
+# MC-CLI
+
 A CLI for setting up and controlling Minecraft servers.
 
 ## DISCLAIMER
@@ -15,3 +16,44 @@ This project is in heavy development still! Most features are either non-existen
 
 ##### Example
 `mc setup spigot -m 1.11.2`
+
+## Getting Started
+
+These instructions will get you up and running with `MC-CLI`.
+
+### Prerequisites
+
+You will need a few prerequisites installed to start. 
+
+* Java JDK
+* Git Bash or Bash on Ubuntu on Windows (Optional)
+* Docker (Recommended but Optional)
+
+### Installing
+
+```
+npm install -g minecraft-cli
+```
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/HexagonMinecraft/mc-cli/tags). 
+
+## Authors
+
+* **Noah Prail** - *Initial work* - [nprail](https://github.com/nprail)
+
+See also the list of [contributors](https://github.com/HexagonMinecraft/mc-cli/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone who's code was used
+* Inspired by [MSCS](https://github.com/MinecraftServerControl/mscs)
--- cli.js
+++ cli.js
@@ -6,8 +6,6 @@
 
 var fs = require('fs');
 var os = require('os');
-var http = require('http');
-var https = require('https');
 var program = require('commander');
 var clc = require('cli-color');
 var pkgv = require('./package.json').version;
@@ -16,88 +14,7 @@ var download = require('download-file')
 var spawn = require('child_process').spawn;
 var platform = os.platform();
 var del = require('del');
-
-var spigotSetup = function (version) {
-    // SpigotConfig
-    var spigotUrl = "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
-
-    var spigotOpts = {
-        directory: ".",
-        filename: "BuildTools.jar"
-    }
-
-    var bashOnUbuntuOnWindows = 'C:\\Windows\\System32\\bash.exe';
-    var winBash64 = 'C:\\Program Files\\Git\\bin\\bash.exe';
-    var winBash86 = 'C:\\Program Files (x86)\\Git\\bin\\bash.exe';
-    var winBash;
-
-    if (platform === "win32") {
-        // Look for Bash.exe
-        fs.stat(bashOnUbuntuOnWindows, function (err, stats) {
-            if (err) {
-                fs.stat(winBash64, function (err, stats) {
-                    if (err) {
-                        fs.stat(winBash64, function (err, stats) {
-                            // If no Bash.exe could be found, throw an error.  
-                            if (err) console.error('Bash.exe could not be found. Please install Git Bash from https://git-scm.com/')
-                            // If 32bit Bash exists, use it. 
-                            else winBash = '"' + winBash86 + '"';
-                        });
-                    }
-                    // If 64bit Bash exists, use it. 
-                    else winBash = '"' + winBash64 + '"';
-                });
-            }
-            // If Bash on Ubuntu On Windows Exists, use it. 
-            else winBash = '"' + bashOnUbuntuOnWindows + '"';
-        });
-    }
-
-    sh.mkdir('tmp');
-    sh.cd('tmp');
-    console.log('Starting Spigot Build');
-
-    download(spigotUrl, spigotOpts, function (err) {
-        if (err) throw err
-
-        var javaCmd = 'java -jar BuildTools.jar --rev ' + version;
-        var cmd;
-
-        if (platform === "win32") {
-            // build on Windows properly
-
-            console.log('Using Bash.exe found at:', winBash);
-            cmd = winBash + ' --login -i -c "' + javaCmd + '"';
-        } else {
-            // build on all other platforms
-            cmd = javaCmd;
-        }
-
-        sh.exec(cmd, function (code, stdout, stderr) {
-            if (code != 0) throw stderr;
-            sh.cd('..');
-            sh.cp('tmp/spigot-' + version + '.jar', 'server.jar')
-
-            del('tmp').then(paths => {
-                console.log('Removed', paths, 'folder');
-            });
-        });
-    });
-
-}
-var vanilaSetup = function (version) {
-    var vanilaUrl = 'https://s3.amazonaws.com/Minecraft.Download/versions/' + version + '/minecraft_server.' + version + '.jar'
-    var vanilaOpts = {
-        directory: ".",
-        filename: "server.jar"
-    }
-    console.log('Starting Vanila Setup');
-
-    download(vanilaUrl, vanilaOpts, function (err) {
-        if (err) throw err
-        console.log('Download Complete')
-    });
-}
+var setup = require('./lib/setup.js');
 
 program
     .version(pkgv)
@@ -111,13 +28,7 @@ program
         var verbose = options.verbose || 'false';
         version = options.minecraft_version || 'latest';
         server = server || 'vanila';
-        console.log('Setting up a %s %s Minecraft server. Verbose: %s', server, version, verbose);
-        if (server === 'spigot') {
-            spigotSetup(version);
-        }
-        if (server === 'vanila') {
-            vanilaSetup(version);
-        }
+        setup(version, server, verbose);
     });
 
 program.parse(process.argv);
