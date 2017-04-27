9d4831df68ec6682a35d7fd1a9fc082f24056b6f
Noah Prail <nprail@filiosoft.com>
2017-04-27 01:35:15 -0400

    Update install-mc-cli.ps1

--- scripts/install-mc-cli.ps1
+++ scripts/install-mc-cli.ps1
@@ -1,7 +1,24 @@
-# Usage: iex (new-object net.webclient).downloadstring('https://git.io/')
+#requires -v 3
+# remote install:
+#   iex (new-object net.webclient).downloadstring('https://git.io/v9sLj')
+$erroractionpreference='stop' # quit if anything goes wrong
 
 echo "Installing deps..."
-iex (new-object net.webclient).downloadstring('https://git.io/v9sLK')
-scoop install nodejs
+if ((Get-Command "scoop" -ErrorAction SilentlyContinue) -eq $null) 
+{ 
+   Write-Host "Installing Scoop..."
+   iex (new-object net.webclient).downloadstring('https://git.io/v9sLK')
+}
+if ((Get-Command "node" -ErrorAction SilentlyContinue) -eq $null) 
+{ 
+   Write-Host "Installing Node.js..."
+   scoop install nodejs
+}
+if ((Get-Command "java" -ErrorAction SilentlyContinue) -eq $null) 
+{ 
+   Write-Host "Installing OpenJDK..."
+   scoop install openjdk
+}
 
+echo "Installing mc-cli..."
 npm install -g minecraft-cli
