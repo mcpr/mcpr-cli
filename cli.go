5bb25cb9689f52c0801afe4accf214eb9b8d5ad8
Noah Prail <nprail@filiosoft.com>
2017-05-09 16:39:23 -0400

    Plugin install with Spigot API mostly done in Go! #2

--- cli.go
+++ cli.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"io"
 	"net/http"
-
+	"net/url"
 	"os/exec"
+	"strconv"
 
 	"time"
 
-	semver "github.com/Masterminds/semver"
+	"encoding/json"
+
+	"github.com/Masterminds/semver"
 	"github.com/briandowns/spinner"
 	"github.com/urfave/cli"
 )
@@ -51,7 +55,6 @@ func downloadFile(filepath string, url string) (err error) {
 	}
 
 	s.Stop()
-	fmt.Println("Download complete!")
 
 	return nil
 }
@@ -86,6 +89,159 @@ func setupBuildTools(serverType, serverVersion string) {
 	os.RemoveAll("tmp")
 }
 
+func setup(ver, serverType string) {
+	v, err := semver.NewVersion(ver)
+	if err != nil {
+		fmt.Println("The version", ver, "doesn't seem to be valid. Are you sure that you specified a valid version?")
+		os.Exit(1)
+	}
+
+	fmt.Println("Starting setup...")
+	fmt.Println("Server Type:", serverType)
+	fmt.Println("Minecraft Version:", v)
+
+	switch {
+	case serverType == "spigot":
+		setupBuildTools(serverType, ver)
+	case serverType == "craftbukkit":
+		setupBuildTools(serverType, ver)
+	case serverType == "vanilla":
+		downloadFile("server.jar", "https://s3.amazonaws.com/Minecraft.Download/versions/"+ver+"/minecraft_server."+ver+".jar")
+	default:
+		fmt.Println("The server type", serverType, " is not supported.")
+	}
+
+	fmt.Println("All done! Run java -jar server.jar to start your server!")
+}
+
+type ResourceArray []struct {
+	Name string `json:"name"`
+	Tag  string `json:"tag"`
+	ID   int    `json:"id"`
+}
+
+type Resource struct {
+	External bool `json:"external"`
+	File     struct {
+		Type     string `json:"type"`
+		Size     int    `json:"size"`
+		SizeUnit string `json:"sizeUnit"`
+		URL      string `json:"url"`
+	} `json:"file"`
+	Versions []struct {
+		ID int `json:"id"`
+	} `json:"versions"`
+	Updates []struct {
+		ID int `json:"id"`
+	} `json:"updates"`
+	Name    string `json:"name"`
+	Tag     string `json:"tag"`
+	Version struct {
+		ID int `json:"id"`
+	} `json:"version"`
+	Author struct {
+		ID int `json:"id"`
+	} `json:"author"`
+	Category struct {
+		ID int `json:"id"`
+	} `json:"category"`
+	ID int `json:"id"`
+}
+
+func spigotAPIClientArray(endpoint string) ResourceArray {
+	spigotAPI := "https://api.spiget.org/v2"
+
+	url := fmt.Sprintf(spigotAPI + endpoint)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		os.Exit(1)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	var recordArray ResourceArray
+
+	if err := json.NewDecoder(resp.Body).Decode(&recordArray); err != nil {
+		log.Println(err)
+	}
+
+	return recordArray
+}
+
+func spigotAPIClient(endpoint string) Resource {
+	spigotAPI := "https://api.spiget.org/v2"
+
+	url := fmt.Sprintf(spigotAPI + endpoint)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		os.Exit(1)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	var record Resource
+
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
+	}
+	return record
+}
+
+func installPlugin(id string) {
+	spigotBaseURL := "https://www.spigotmc.org/"
+
+	safeID := url.QueryEscape(id)
+	endpoint := "/resources/" + safeID
+
+	req := spigotAPIClient(endpoint)
+	url := spigotBaseURL + req.File.URL
+	if req.File.Type == "external" {
+		fmt.Println("This plugin seems to be from an external source. Please visit the following site to download it:", url)
+		os.Exit(1)
+	}
+	if req.File.Type != ".jar" {
+		fmt.Println("This resouce is not a .jar file. Are you sure it is actually a plugin?")
+		os.Exit(1)
+	}
+	fmt.Println("Installing plugin", req.Name+"...")
+
+	size := strconv.Itoa(req.File.Size)
+	fmt.Println("Download Size:", size+req.File.SizeUnit)
+
+	downloadLocation := "plugins/" + req.Name + ".jar"
+	createTmp("plugins")
+	downloadFile(downloadLocation, url)
+
+	fmt.Println("Installation complete! Restart your Minecraft server now!")
+}
+
+func searchPlugins(name string) {
+	fmt.Println("Searching for plugin", name+"...")
+
+	safeName := url.QueryEscape(name)
+	endpoint := "/search/resources/" + safeName
+
+	req := spigotAPIClientArray(endpoint)
+	fmt.Println(req)
+
+	for i := 1; i < len(req); i += 4 {
+		v := req[i]
+		fmt.Println("\n\nName:", v.Name, "\nID:", v.ID, "\nDescription:", v.Tag, "\nInstall Command: mc install", v.ID)
+	}
+}
+
 func main() {
 	app := cli.NewApp()
 
@@ -107,28 +263,37 @@ func main() {
 					ver = c.Args().Get(1)
 				}
 
-				v, err := semver.NewVersion(ver)
-				if err != nil {
-					fmt.Println("The version", ver, "doesn't seem to be valid. Are you sure that you specified a valid version?")
-					return nil
+				setup(ver, serverType)
+				return nil
+			},
+		},
+		{
+			Name:    "install",
+			Aliases: []string{"i"},
+			Usage:   "Install a plugin or plugins - mc install [plugin]",
+			Action: func(c *cli.Context) error {
+				if c.Args().Get(0) != "" {
+					pluginID := c.Args().Get(0)
+					installPlugin(pluginID)
+				} else {
+					fmt.Println("Installing all plugins...")
 				}
 
-				fmt.Println("Starting setup...")
-				fmt.Println("Server Type:", serverType)
-				fmt.Println("Minecraft Version:", v)
-
-				switch {
-				case serverType == "spigot":
-					setupBuildTools(serverType, ver)
-				case serverType == "craftbukkit":
-					setupBuildTools(serverType, ver)
-				case serverType == "vanilla":
-					downloadFile("server.jar", "https://s3.amazonaws.com/Minecraft.Download/versions/"+ver+"/minecraft_server."+ver+".jar")
-				default:
-					fmt.Println("The server type", serverType, " is not supported.")
+				return nil
+			},
+		},
+		{
+			Name:    "search",
+			Aliases: []string{"l"},
+			Usage:   "Search plugins - mc search [plugin]",
+			Action: func(c *cli.Context) error {
+				if c.Args().Get(0) != "" {
+					pluginName := c.Args().Get(0)
+					searchPlugins(pluginName)
+				} else {
+					fmt.Println("Please specify a plugin.")
 				}
 
-				fmt.Println("All done! Run java -jar server.jar to start your server!")
 				return nil
 			},
 		},
