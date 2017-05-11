16c8fe7a7ab63f6d07708c3eda288814b83cf413
Noah Prail <nprail@filiosoft.com>
2017-05-11 13:52:47 -0400

    Improved install script more.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,39 +5,57 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 BASE_URL=https://artifacts.filiosoft.com/mc-cli
-
 USR_BIN=/usr/local/bin/mc
 
-echo "You are running $(uname)"
+do_install (){
+  echo "Installing:"
+  cat <<-EOF
+    __  __  ____       ____ _     ___
+  |  \/  |/ ___|     / ___| |   |_ _|
+  | |\/| | |   _____| |   | |    | |
+  | |  | | |__|_____| |___| |___ | |
+  |_|  |_|\____|     \____|_____|___|
 
-function installMc {
-  if [ "$(uname)" == "Darwin" ]; then
-    URL=$BASE_URL/darwin/mc
-    echo "Downloading binaries from $URL"
-    curl -sO $URL
-  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    URL=$BASE_URL/linux/mc
-    echo "Downloading binary from $URL"
-    curl -sO $URL
+
+	EOF
+  echo "You are running $(uname)"
+
+  installMc () {
+    if [ "$(uname)" == "Darwin" ]; then
+      URL=$BASE_URL/darwin/mc
+      echo "Downloading binaries from $URL"
+      curl -sO $URL
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+      URL=$BASE_URL/linux/mc
+      echo "Downloading binary from $URL"
+      curl -sO $URL
+    fi
+    echo "Moving binary to $USR_BIN"
+    mv mc $USR_BIN
+    chmod +x $USR_BIN
+
+    VERSION=$(mc --version)
+    printf  "\n$VERSION has been installed!"
+    printf "\nRun mc --help for usage information.\n"
+  }
+
+  if [ "type mc" ];
+  then
+    echo -n "The command mc already exists on your system. Do you want to continue? (y/n)? "
+    read answer
+    if echo "$answer" | grep -iq "^y" ;then
+      installMc
+    else
+      echo "Install canceled."
+      exit
+    fi
+  else
+    installMc
   fi
-  mv mc $USR_BIN
-  chmod +x $USR_BIN
-
-  VERSION=$(mc --version)
-  echo "$VERSION has been installed!"
-  echo "Run mc --help for usage information."
 }
 
-if [ -e "$USR_BIN" ]
-then
-  echo "A file at $USR_BIN already exists."
-  echo -n "Do you want to overwrite it? (y/n)? "
-  read answer
-  if echo "$answer" | grep -iq "^y" ;then
-    installMc
-  else
-    exit
-  fi
-else
-  installMc
-fi
+
+# wrapped up in a function so that we have some protection against only getting
+# half the file during "curl | sh"
+
+do_install
