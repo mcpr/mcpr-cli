0e7b894ba5d8c75253ccf96ed973f012857c6010
Noah Prail <nprail@filiosoft.com>
2017-05-11 15:30:29 -0400

    Improvements to install script [ci skip]

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,46 +7,59 @@ fi
 BASE_URL=https://artifacts.filiosoft.com/mc-cli
 USR_BIN=/usr/local/bin/mc
 
+# Colors
+COLOREND=$(tput sgr0)
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+YELLOW=$(tput setaf 3)
+RED=$(tput setaf 1)
+UNDER=$(tput smul)
+BOLD=$(tput bold)
+
 do_install (){
   echo "Installing:"
+
   cat <<-EOF
+  ${GREEN}
     __  __  ____       ____ _     ___
   |  \/  |/ ___|     / ___| |   |_ _|
   | |\/| | |   _____| |   | |    | |
   | |  | | |__|_____| |___| |___ | |
   |_|  |_|\____|     \____|_____|___|
-
-
+  ${COLOREND}
 	EOF
-  echo "You are running $(uname)"
+
+  sleep 3
+
+  echo "${BLUE}You are running $(uname) $(uname -m)${COLOREND}"
 
   installMc () {
     if [ "$(uname)" == "Darwin" ]; then
       URL=$BASE_URL/darwin/mc
-      echo "Downloading binaries from $URL"
+      echo "${BLUE}Downloading binaries from $URL${COLOREND}"
       curl -sO $URL
     elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       URL=$BASE_URL/linux/mc
-      echo "Downloading binary from $URL"
+      echo "${BLUE}Downloading binaries from $URL${COLOREND}"
       curl -sO $URL
     fi
-    echo "Moving binary to $USR_BIN"
+    echo "${BLUE}Moving binary to $USR_BIN${COLOREND}"
     mv mc $USR_BIN
     chmod +x $USR_BIN
 
     VERSION=$(mc --version)
-    printf  "\n$VERSION has been installed!"
-    printf "\nRun mc --help for usage information.\n"
+    printf  "${GREEN}\n$VERSION has been installed!${COLOREND}"
+    printf "\n${BOLD}${UNDER}Run mc --help for usage information.\n${COLOREND}"
   }
 
   if [ "type mc" ];
   then
-    echo -n "The command mc already exists on your system. Do you want to continue? (y/n)? "
+    echo -n "${YELLOW}The command mc already exists on your system. Do you want to continue? (y/n)? ${COLOREND}"
     read answer
     if echo "$answer" | grep -iq "^y" ;then
       installMc
     else
-      echo "Install canceled."
+      echo "${RED}Install canceled.${COLOREND}"
       exit
     fi
   else
