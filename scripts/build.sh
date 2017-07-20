6241ea64052756d30dfd4180019ea64a941046a4
Noah Prail <nprail@filiosoft.com>
2017-07-19 22:06:39 -0400

    Fixed GPG

--- scripts/build.sh
+++ scripts/build.sh
@@ -71,10 +71,9 @@ cat <<EOT > ~/.aptly.conf
 EOT
 
 gpg --allow-secret-key-import --import private.key
-echo ${GPG_KEY_PWD} > pwd.txt
-cat pwd.txt
+gpg --list-secret-keys
 
 aptly repo create -distribution=squeeze -component=main mc-cli-release
 aptly repo add mc-cli-release bin/linux/
 aptly snapshot create mc-cli-$VERSION_NAME from repo mc-cli-release
-aptly publish snapshot -batch -gpg-key="C4B1ED8C" -passphrase=${GPG_KEY_PWD} -architectures="i386,amd64,all" mc-cli-${VERSION_NAME} s3:apt.filiosoft.com:
+aptly publish snapshot -batch -gpg-key="D235F87C" -passphrase=${GPG_KEY_PWD} -architectures="i386,amd64,all" mc-cli-${VERSION_NAME} s3:apt.filiosoft.com:
