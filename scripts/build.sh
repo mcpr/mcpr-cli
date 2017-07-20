6f6e12ea281d2a96ab99c54b6ed4a753324f8063
Noah Prail <nprail@filiosoft.com>
2017-07-19 22:15:39 -0400

    Moved pass

--- scripts/build.sh
+++ scripts/build.sh
@@ -72,8 +72,9 @@ EOT
 
 gpg --allow-secret-key-import --import private.key
 gpg --list-secret-keys
+echo ${GPG_KEY_PWD} > pwd.txt
 
 aptly repo create -distribution=squeeze -component=main mc-cli-release
 aptly repo add mc-cli-release bin/linux/
 aptly snapshot create mc-cli-$VERSION_NAME from repo mc-cli-release
-aptly publish snapshot -batch -gpg-key="D235F87C" -passphrase=${GPG_KEY_PWD} -architectures="i386,amd64,all" mc-cli-${VERSION_NAME} s3:apt.filiosoft.com:
+aptly publish snapshot -batch -gpg-key="C4B1ED8C" -passphrase-file="pwd.txt" -architectures="i386,amd64,all" mc-cli-${VERSION_NAME} s3:apt.filiosoft.com:
