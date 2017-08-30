796cab8ca70ad1e429de9e3d2c5e3517c53c53e4
Noah Prail <nprail@filiosoft.com>
2017-08-30 13:56:33 -0400

    Use different GPG key for apt.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -33,4 +33,4 @@ fi
 aptly repo create -distribution=${DISTRIBUTION} -comment="${COMMENT}" -component=main mcpr-cli-release
 aptly repo add mcpr-cli-release bin/${LATEST_PREFIX}/linux/${1}
 aptly snapshot create mcpr-cli-${1} from repo mcpr-cli-release
-aptly publish snapshot -batch=true -gpg-key="C4B1ED8C" -architectures="i386,amd64,all" mcpr-cli-${1} s3:apt.filiosoft.com:
+aptly publish snapshot -batch=true -gpg-key="F56BD64C" -passphrase="$GPG_PWD" -architectures="i386,amd64,all" mcpr-cli-${1} s3:apt.filiosoft.com:
