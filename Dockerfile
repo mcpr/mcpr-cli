e26db1dbc54a452582409b0068f46b33daca96b8
Noah Prail <nprail@filiosoft.com>
2017-09-21 19:48:55 -0400

    Fixed travis deploy!

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ RUN apt-get update && \
     apt-get install --no-install-recommends -y curl apt-transport-https default-jdk git && \
     rm -rf /var/lib/apt/lists/*
 
-RUN echo "deb https://apt.filiosoft.com/debian/ nightly main" > /etc/apt/sources.list.d/filiosoft.list && \
-    curl -sk https://apt.filiosoft.com/debian/pubkey.gpg | apt-key add - && \
+RUN echo "deb https://get.mcpr.io/debian/ nightly main" > /etc/apt/sources.list.d/mcpr.list && \
+    curl -sk https://get.mcpr.io/debian/pubkey.gpg | apt-key add - && \
     apt-get update && \
     apt-get install --no-install-recommends -y mcpr-cli && \
     rm -rf /var/lib/apt/lists/*
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,47 +1,52 @@
 #!/bin/bash
 
-echo -e "\n=============\nPublish Stage\n=============\n"
-
-VERSION=$(cat version.txt)
-
-if [ -z "$TRAVIS_TAG" ]
-then
-    echo "Not Tag"
-    VERSION_NAME=${VERSION}-${TRAVIS_BUILD_NUMBER}
-    LATEST_PREFIX="nightly"
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    echo "Doing nothing on OSX"
 else
-    echo "Building Tag"
-    VERSION_NAME=${TRAVIS_TAG}-${TRAVIS_BUILD_NUMBER}
-    LATEST_PREFIX="stable"
-fi
+    echo -e "\n=============\nPublish Stage\n=============\n"
 
-cat <<EOT > ~/.aptly.conf
-{
-   "S3PublishEndpoints":{
-      "get.mcpr.io":{
-         "region":"us-east-2",
-         "bucket":"get.mcpr.io",
-         "prefix":"debian",
-         "acl":"public-read"
-      }
-   }
-}
+    VERSION=$(cat version.txt)
+
+    if [ -z "$TRAVIS_TAG" ]
+    then
+        echo "Not Tag"
+        VERSION_NAME=${VERSION}-${TRAVIS_BUILD_NUMBER}
+        LATEST_PREFIX="nightly"
+    else
+        echo "Building Tag"
+        VERSION_NAME=${TRAVIS_TAG}-${TRAVIS_BUILD_NUMBER}
+        LATEST_PREFIX="stable"
+    fi
+
+    cat <<EOT > ~/.aptly.conf
+    {
+        "S3PublishEndpoints":{
+            "get.mcpr.io":{
+                "region":"us-east-2",
+                "bucket":"get.mcpr.io",
+                "prefix":"debian",
+                "acl":"public-read"
+            }
+        }
+    }
 EOT
+    # wget https://get.mcpr.io/debian/pubkey.gpg
 
-wget https://get.mcpr.io/debian/pubkey.gpg
+    if [ -z "$TRAVIS_TAG" ]
+    then
+        DISTRIBUTION=nightly
+        COMMENT="Nightly builds"
+        echo -e "\nNightly build\n"
+    else
+        DISTRIBUTION=stable
+        COMMENT="Stable builds"
+        echo -e "\nStable build\n"
+    fi
+    echo -e "\nPublishing v${VERSION_NAME}-${LATEST_PREFIX}\n"
 
-if [ -z "$TRAVIS_TAG" ]
-then
-    DISTRIBUTION=nightly
-    COMMENT="Nightly builds"
-    echo -e "\nNightly build\n"
-else
-    DISTRIBUTION=stable
-    COMMENT="Stable builds"
-    echo -e "\nStable build\n"
+    aptly repo create -distribution=${DISTRIBUTION} -comment="${COMMENT}" -component=main mcpr-cli-release
+    aptly repo add mcpr-cli-release bin/${LATEST_PREFIX}/linux/${VERSION_NAME}
+    aptly snapshot create mcpr-cli-${VERSION_NAME} from repo mcpr-cli-release
+    aptly publish snapshot -batch=true -gpg-key="F56BD64C" -passphrase="$GPG_PWD" -architectures="i386,amd64,all" mcpr-cli-${VERSION_NAME} s3:get.mcpr.io:
+
 fi
-
-aptly repo create -distribution=${DISTRIBUTION} -comment="${COMMENT}" -component=main mcpr-cli-release
-aptly repo add mcpr-cli-release bin/${LATEST_PREFIX}/linux/${VERSION_NAME}
-aptly snapshot create mcpr-cli-${VERSION_NAME} from repo mcpr-cli-release
-aptly publish snapshot -batch=true -gpg-key="F56BD64C" -passphrase="$GPG_PWD" -architectures="i386,amd64,all" mcpr-cli-${VERSION_NAME} s3:get.mcpr.io:
