af93db1f092ed04283bbafe9cd2ad26f08ad39d4
Noah Prail <nprail@filiosoft.com>
2017-09-21 22:25:14 -0400

    LS *.pkg

--- scripts/build.sh
+++ scripts/build.sh
@@ -100,6 +100,7 @@ then
   security unlock-keychain -p travis $KEY_CHAIN
 
   echo -e "\nSigning OSX PKG"
+  ls -la *.pkg
   productsign --sign '3rd Party Mac Developer Installer: Filiosoft, LLC (U2PJ8B6P8N)' mcpr*.pkg mcpr-cli-signed.pkg
   cp mcpr-cli-signed.pkg bin/darwin/mcpr-cli-latest.pkg
   mv mcpr-cli-signed.pkg bin/darwin/${VERSION_NAME}/mcpr-cli-${VERSION_NAME}.pkg
